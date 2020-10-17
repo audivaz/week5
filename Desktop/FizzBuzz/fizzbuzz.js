0e8e398b5885001224529c3ba24c4e4cf3e45a7c
audivaz <audivaz3@gmail.com>
2020-10-17 13:02:02 -0400

    added html and javascript

---
+++ Desktop/FizzBuzz/fizzbuzz.js
@@ -0,0 +1,25 @@
+for (let i = 1; i < 101; i++) {
+    console.log(i);
+}
+
+let sample = 'fizz'; 
+if(sample / 4){ 
+    console.log('fizz'); 
+}else{ 
+    console.log('no'); 
+} 
+
+let sample2 = 'buzz'; 
+if(sample2 / 5){ 
+    console.log('buzz'); 
+}else{ 
+    console.log('no'); 
+} 
+
+
+let sample3 = 'FizzBuzz'; 
+if(sample3 / 5){ 
+    console.log('FizzBuzz'); 
+}else{ 
+    console.log('no'); 
+} 
