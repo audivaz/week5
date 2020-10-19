876277bb30164457f0dc8ee1b7588d300b73f437
audivaz <audivaz3@gmail.com>
2020-10-19 13:04:31 -0400

    new javascript

--- Desktop/FizzBuzz/fizzbuzz.js
+++ Desktop/FizzBuzz/fizzbuzz.js
@@ -1,25 +1,16 @@
-for (let i = 1; i < 101; i++) {
-    console.log(i);
+
+
+
+var i;
+
+for(i=0; i < 101; i++) {
+    if(i%3 === 0 && i%5 === 0) {
+        console.log('fizzbuzz');
+    } else if(i%3 === 0) {
+        console.log('fizz');
+    } else if(i%5 === 0) {
+        console.log('buzz');
+    } else {
+        console.log(i);
+    }
 }
-
-let sample = 'fizz'; 
-if(sample / 4){ 
-    console.log('fizz'); 
-}else{ 
-    console.log('no'); 
-} 
-
-let sample2 = 'buzz'; 
-if(sample2 / 5){ 
-    console.log('buzz'); 
-}else{ 
-    console.log('no'); 
-} 
-
-
-let sample3 = 'FizzBuzz'; 
-if(sample3 / 5){ 
-    console.log('FizzBuzz'); 
-}else{ 
-    console.log('no'); 
-} 
